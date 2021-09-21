f11b82fcd53609d0d109e9c959521984d30a2390
Nirali D Bafna <niralideepakbafna@gmail.com>
2021-09-21 20:37:57 +0530

    Program 27: Write a program to return the ascii value for a character.

---
+++ 27.c
@@ -0,0 +1,10 @@
+//ASCII value
+#include<stdio.h>
+int main()
+{
+    int ch;
+    printf("Enter a character: ");
+    scanf("%c",&ch);
+    printf("ASCII value of %c is %d\n",ch,ch);
+    return 0;
+}
